9507f9902525c62890ba04058beeff1b897177fe
Luka <alutza94@gmail.com>
2025-11-14 20:28:45 +0100

    g

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Github, ExternalLink } from "lucide-react";
 import { Navigation } from "../components/nav";
-// abcdef
+// abcdefg
 import {
   SiNextdotjs,
   SiTailwindcss,
